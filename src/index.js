db92567b2125cfa6baedf32f26a3b1cc9fa4ffc7
Glenzone <glennsonmadeja@gmail.com>
2023-01-12 20:20:01 +0800

    updated index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-console.log(path.join(__dirname,'../public'));
+// console.log(path.join(__dirname,'../public'));
 
-app.use(express.static(path.join(__dirname,'../public')));
+// app.use(express.static(path.join(__dirname,'../public')));
 
 app.get('', (req, res) => {
     res.send('Hello world!');
